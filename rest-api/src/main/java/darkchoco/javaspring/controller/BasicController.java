222db557f0efd3e75a7945f4ad56167690f8f4d1
darkchoco <darkchoco@cloudscape.kr>
2022-03-22 14:37:21 +0100

    main: [rest-api] HTTP methods added with many tests.

--- rest-api/src/main/java/darkchoco/javaspring/controller/BasicController.java
+++ rest-api/src/main/java/darkchoco/javaspring/controller/BasicController.java
@@ -1,9 +1,11 @@
 package darkchoco.javaspring.controller;
 
 import darkchoco.javaspring.model.Todo;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -12,8 +14,30 @@ public class BasicController {
 
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = "/todo")
+    @GetMapping(value = "/todo")
     public Todo basic() {
         return new Todo(counter.incrementAndGet(), "Book the flight");
+    }
+
+    @PostMapping(value = "/todo-post")
+    public Todo basicPost(@RequestParam(value = "todoTitle") String todoTitle) {
+        return new Todo(counter.incrementAndGet(), todoTitle);
+    }
+
+    @PostMapping(value = "/todo-entity")
+    public ResponseEntity<Todo> basicPostResponseEntity(
+            @RequestParam(value = "todoTitle") String todoTitle) {
+        return new ResponseEntity<>(
+                new Todo(counter.incrementAndGet(), todoTitle), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/todos/{todoId}")
+    public Todo getPath(@PathVariable int todoId) {
+        Map<Integer, Todo> todoMap =
+                Map.of(1, new Todo(1L, "Book the flight"),
+                       2, new Todo(2L, "Book the hotel"),
+                       3, new Todo(3L, "Check the itinerary"));
+
+        return todoMap.get(todoId);
     }
 }
--- rest-api/src/test/java/darkchoco/javaspring/controller/BasicControllerTest.java
+++ rest-api/src/test/java/darkchoco/javaspring/controller/BasicControllerTest.java
@@ -1,11 +1,18 @@
 package darkchoco.javaspring.controller;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,12 +25,59 @@ public class BasicControllerTest {
 
     @Test
     public void basic() {
-        client.get().uri("/basic/todo").exchange()
+        client.get().uri("/basic/todo")
+                .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
                 .expectBody(String.class)
                 .consumeWith(exchangeResult -> assertThat(exchangeResult.getResponseBody())
                         .isNotNull()
                         .contains("Book the flight"));
+    }
+
+    @Test
+    void basicPost() {
+        MultiValueMap<String, String> bodyValues = new LinkedMultiValueMap<>();
+
+        bodyValues.add("todoTitle", "Book the hotel");
+
+        client.post().uri("/basic/todo-post").body(BodyInserters.fromFormData(bodyValues))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .consumeWith(stringEntityExchangeResult -> assertThat(stringEntityExchangeResult.getResponseBody())
+                        .isNotNull()
+                        .contains("Book the hotel"));
+    }
+
+    @Test
+    void basicPostResponseEntity() {
+        MultiValueMap<String, String> bodyValues = new LinkedMultiValueMap<>();
+
+        bodyValues.add("todoTitle", "Book the hotel");
+
+        client.post().uri("/basic/todo-post").body(BodyInserters.fromFormData(bodyValues))
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void getPath(int param) {
+        client.get().uri("/basic/todos/" + param)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+                .expectBody(String.class)
+                .consumeWith(exchangeResult -> {
+                    assertThat(exchangeResult.getResponseBody())
+                            .isNotNull()
+                            .containsAnyOf("flight", "hotel", "itinerary");
+                    try {
+                        JSONAssert.assertEquals("{id:" + param + "}", exchangeResult.getResponseBody(), false);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
